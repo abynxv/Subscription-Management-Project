237297d20b65f3d1f6bf03989faa93aaf32bc705
Abhinav P K <abhinav.pk@hotmail.com>
2025-08-27 05:28:54 +0530

    Update apps.py

--- subscription_management_backend/auth_app/apps.py
+++ subscription_management_backend/auth_app/apps.py
@@ -1,29 +1,24 @@
-from django.apps import AppConfig
-
-
-class AuthAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'auth_app'
 # auth_app/apps.py
 from django.apps import AppConfig
-from django.conf import settings
+from django.db.models.signals import post_migrate
+from django.contrib.auth import get_user_model
 
 class AuthAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auth_app'
 
     def ready(self):
-        # Avoid running multiple times in migrations or tests
-        if settings.RUN_CREATE_ADMIN_ON_STARTUP:
-            from django.contrib.auth import get_user_model
+        from django.conf import settings
 
+        def create_admin(sender, **kwargs):
             User = get_user_model()
-            email = "admin@sm.com"
+            email = 'admin@sm.com'
             if not User.objects.filter(email=email).exists():
                 User.objects.create_superuser(
                     email=email,
-                    username="admin",
-                    password="admin",
-                    role="admin"
+                    username='admin',
+                    password='admin',
+                    role='admin'
                 )
 
+        post_migrate.connect(create_admin, sender=self)
