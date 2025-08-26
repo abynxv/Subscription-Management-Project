a409ea663003413595403f0c00c1ce2f07312026
Abhinav P K <abhinav.pk@hotmail.com>
2025-08-27 05:21:46 +0530

    Update apps.py

--- subscription_management_backend/auth_app/apps.py
+++ subscription_management_backend/auth_app/apps.py
@@ -4,3 +4,26 @@ from django.apps import AppConfig
 class AuthAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auth_app'
+# auth_app/apps.py
+from django.apps import AppConfig
+from django.conf import settings
+
+class AuthAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'auth_app'
+
+    def ready(self):
+        # Avoid running multiple times in migrations or tests
+        if settings.RUN_CREATE_ADMIN_ON_STARTUP:
+            from django.contrib.auth import get_user_model
+
+            User = get_user_model()
+            email = "admin@sm.com"
+            if not User.objects.filter(email=email).exists():
+                User.objects.create_superuser(
+                    email=email,
+                    username="admin",
+                    password="admin",
+                    role="admin"
+                )
+
