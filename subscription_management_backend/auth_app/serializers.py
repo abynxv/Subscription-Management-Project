c5ce0728f3a0bd6838990ea747670db219574c9f
Abhinav P K <abhinav.pk@hotmail.com>
2025-08-27 19:32:43 +0530

    Update serializers.py

--- subscription_management_backend/auth_app/serializers.py
+++ subscription_management_backend/auth_app/serializers.py
@@ -5,7 +5,7 @@ from .models import CustomUser
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ["id", "username", "password", "email"]
+        fields = ["id", "username", "password", "email", "role"]
         extra_kwargs = {
             "password": {"write_only": True}
         }
